7e147947f6677623a2b16067e796bb76dff6e610
Reese M Bowman <reesemb1@gmail.com>
2019-12-16 18:44:11 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,7 +17,6 @@ end
 
 def my_hash_creator(key, value)
   hash = {key => value}
-  # return a hash that includes the key and value parameters passed into this method
 end
 
 def read_from_hash(hash, key)
